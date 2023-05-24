a90015bced0774b521f489193281d477a4e5dd17
Oli Evans <oli@protocol.ai>
2023-05-24 16:00:06 +0100

    feat: streamier
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- packages/lambda/src/autobahn.js
+++ packages/lambda/src/autobahn.js
@@ -1,3 +1,8 @@
+import { Readable } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import { streamifyResponse } from 'lambda-stream'
+import autobahn from '@web3-storage/autobahn-core/index.js'
+
 // import * as Sentry from '@sentry/serverless'
 
 // Sentry.AWSLambda.init({
@@ -6,47 +11,101 @@
 //   tracesSampleRate: 1.0
 // })
 
+const text = 'text/plain; charset=utf-8'
+const json = 'application/json'
+
 /**
- * AWS HTTP Gateway handler for GET /version
- *
- * @param {import('aws-lambda').APIGatewayProxyEventV2} request
+ * Main autobahn catch all handler / router
+ * Warning: not a real proxy event. is a lambda url invocation
+ * @param {import('aws-lambda').APIGatewayProxyEventV2} event
+ * @param {import('lambda-stream').ResponseStream} res
+ * @param {import('aws-lambda').Context} ctx
  */
-export async function getVersion (request) {
-  const { NAME: name, VERSION: version, COMMIT: commit, STAGE: env, REPO: repo } = process.env
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ name, version, repo, commit, env })
+export async function _handler (event, res, ctx) {
+  const rawPath = event.rawPath
+  if (rawPath.startsWith('/ipfs/')) {
+    return getIpfs(event, res)
   }
+  if (rawPath === '/version' || rawPath === '/version/') {
+    return getVersion(event, res)
+  }
+  if (rawPath === '/error' || rawPath === '/error/') {
+    throw new Error('/error deliberate error')
+  }
+  if (rawPath === '' || rawPath === '/') {
+    return getHome(event, res)
+  }
+  return get404(event, res, ctx)
 }
 
-// export const version = Sentry.AWSLambda.wrapHandler(getVersion)
+export const handler = streamifyResponse(_handler)
 
 /**
- * AWS HTTP Gateway handler for GET /
- *
- * @param {import('aws-lambda').APIGatewayProxyEventV2} request
+ * @param {import('aws-lambda').APIGatewayProxyEventV2} evt
+ * @param {import('lambda-stream').ResponseStream} res
  */
-export async function getHome (request) {
+export async function getIpfs (evt, res) {
+  const url = new URL(evt.rawPath || '', `http://${evt.headers.host}`)
+  const headers = new Headers(Object.entries(Headers))
+  const method = evt.requestContext.http.method
+  const body = undefined
+  const request = new Request(url, { method, headers, body })
+  const env = { DEBUG: process.env.DEBUG ?? 'false' }
+  const ctx = { waitUntil: () => {} }
+  // @ts-expect-error
+  const response = await autobahn.fetch(request, env, ctx)
+
+  const contentType = response.headers.get('content-type')
+  if (contentType) {
+    res.setContentType(contentType)
+  }
+
+  // @ts-expect-error body may be undefined
+  await pipeline(Readable.fromWeb(response.body), res)
+}
+
+/**
+ * handler for GET /version
+ * @param {import('aws-lambda').APIGatewayProxyEventV2} evt
+ * @param {import('lambda-stream').ResponseStream} res
+ */
+export async function getVersion (evt, res) {
+  const { NAME: name, VERSION: version, COMMIT: commit, STAGE: env, REPO: repo } = process.env
+  const body = JSON.stringify({ name, version, repo, commit, env })
+  res.setContentType(json)
+  res.write(body)
+  res.end()
+}
+
+/**
+ * handler for GET /
+ * @param {import('aws-lambda').APIGatewayProxyEventV2} evt
+ * @param {import('lambda-stream').ResponseStream} res
+ */
+export async function getHome (evt, res) {
   const { VERSION, BRANCH, STAGE, REPO } = process.env
   const env = STAGE === 'prod' ? '' : `(${STAGE})`
   const repo = BRANCH === 'main' ? REPO : `${REPO}/tree/${BRANCH}`
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/plain; charset=utf-8'
-    },
-    body: `⁂ autobahn v${VERSION} ${env}\n- ${repo}`
-  }
+  const body = `⁂ autobahn v${VERSION} ${env}\n- ${repo}`
+  res.setContentType(text)
+  res.write(body)
+  res.end()
 }
 
 /**
- * AWS HTTP Gateway handler for GET /error
+ * @param {import('aws-lambda').APIGatewayProxyEventV2} evt
+ * @param {import('lambda-stream').ResponseStream} res
+ * @param {import('aws-lambda').Context} ctx
  */
-export async function getError () {
-  throw new Error('API Error')
+export async function get404 (evt, res, ctx) {
+  // res.setContentType(text)
+  // // @ts-expect-error
+  // res.statusCode = 404
+  // res.write('\n 404 🦖 \n')
+  const error = new Error('Not Found')
+  // @ts-expect-error
+  error.statusCode = 404
+  ctx.fail(error)
 }
 
 // export const home = Sentry.AWSLambda.wrapHandler(homeGet)
--- stacks/api.js
+++ stacks/api.js
@@ -1,4 +1,4 @@
-import { Api } from 'sst/constructs'
+import { Function } from 'sst/constructs'
 import { getCustomDomain, getApiPackageJson, getGitInfo } from './config.js'
 
 /**
@@ -6,36 +6,62 @@ import { getCustomDomain, getApiPackageJson, getGitInfo } from './config.js'
  */
 export function API ({ stack }) {
   const SENTRY_DSN = process.env.SENTRY_DSN ?? ''
-  const HOSTED_ZONE = process.env.HOSTED_ZONE
-  const customDomain = getCustomDomain(stack.stage, HOSTED_ZONE)
+  // const HOSTED_ZONE = process.env.HOSTED_ZONE
+  // // const customDomain = getCustomDomain(stack.stage, HOSTED_ZONE)
   const pkg = getApiPackageJson()
   const git = getGitInfo()
-  const api = new Api(stack, 'api', {
-    customDomain,
-    defaults: {
-      function: {
-        environment: {
-          SENTRY_DSN,
-          NAME: pkg.name,
-          REPO: pkg.homepage,
-          VERSION: pkg.version,
-          BRANCH: git.branch,
-          COMMIT: git.commit,
-          STAGE: stack.stage
-        },
-        runtime: 'nodejs18.x',
-        architecture: 'arm_64',
-        timeout: '30 seconds' // http api max response delay is 30s see: https://docs.aws.amazon.com/apigateway/latest/developerguide/limits.html
-      }
+  // const api = new Api(stack, 'api', {
+  //   customDomain,
+  //   defaults: {
+  //     function: {
+  //       environment: {
+  //         SENTRY_DSN,
+  //         NAME: pkg.name,
+  //         REPO: pkg.homepage,
+  //         VERSION: pkg.version,
+  //         BRANCH: git.branch,
+  //         COMMIT: git.commit,
+  //         STAGE: stack.stage
+  //       },
+  //       runtime: 'nodejs18.x',
+  //       architecture: 'arm_64',
+  //       timeout: '30 seconds' // http api max response delay is 30s see: https://docs.aws.amazon.com/apigateway/latest/developerguide/limits.html
+  //     }
+  //   },
+  //   routes: {
+  //     'GET /version': 'packages/lambda/src/autobahn.getVersion',
+  //     'GET /error': 'packages/lambda/src/autobahn.getError',
+  //     'GET /': 'packages/lambda/src/autobahn.getHome'
+  //   }
+  // })
+  stack.setDefaultFunctionProps({
+    environment: {
+      SENTRY_DSN,
+      NAME: pkg.name,
+      REPO: pkg.homepage,
+      VERSION: pkg.version,
+      BRANCH: git.branch,
+      COMMIT: git.commit,
+      STAGE: stack.stage
     },
-    routes: {
-      'GET /version': 'packages/lambda/src/autobahn.getVersion',
-      'GET /error': 'packages/lambda/src/autobahn.getError',
-      'GET /': 'packages/lambda/src/autobahn.getHome'
+    memorySize: '1 GB',
+    runtime: 'nodejs18.x',
+    architecture: 'arm_64',
+    timeout: '15 minutes'
+  })
+
+  // Gotta us lambda fn url + cloudfront to get the streaming behavior we want
+  // see: https://aws.amazon.com/blogs/compute/introducing-aws-lambda-response-streaming/
+  const fn = new Function(stack, 'fn', {
+    handler: 'packages/lambda/src/autobahn.handler',
+
+    url: {
+      cors: true,
+      authorizer: 'none'
     }
   })
+
   stack.addOutputs({
-    Domain: customDomain ? `https://${customDomain.domainName}` : 'Set HOSTED_ZONE in env to deploy to a custom domain',
-    Api: api.url
+    URL: fn.url
   })
 }
