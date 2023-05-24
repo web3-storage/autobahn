248889c5a8f0c7f2567069b7125bf3eb4d478952
Oli Evans <oli@protocol.ai>
2023-05-24 16:05:05 +0100

    chore: tagging
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- stacks/api.js
+++ stacks/api.js
@@ -1,39 +1,13 @@
 import { Function } from 'sst/constructs'
-import { getCustomDomain, getApiPackageJson, getGitInfo } from './config.js'
+import { getApiPackageJson, getGitInfo } from './config.js'
 
 /**
  * @param {import('sst/constructs').StackContext} config
  */
 export function API ({ stack }) {
   const SENTRY_DSN = process.env.SENTRY_DSN ?? ''
-  // const HOSTED_ZONE = process.env.HOSTED_ZONE
-  // // const customDomain = getCustomDomain(stack.stage, HOSTED_ZONE)
   const pkg = getApiPackageJson()
   const git = getGitInfo()
-  // const api = new Api(stack, 'api', {
-  //   customDomain,
-  //   defaults: {
-  //     function: {
-  //       environment: {
-  //         SENTRY_DSN,
-  //         NAME: pkg.name,
-  //         REPO: pkg.homepage,
-  //         VERSION: pkg.version,
-  //         BRANCH: git.branch,
-  //         COMMIT: git.commit,
-  //         STAGE: stack.stage
-  //       },
-  //       runtime: 'nodejs18.x',
-  //       architecture: 'arm_64',
-  //       timeout: '30 seconds' // http api max response delay is 30s see: https://docs.aws.amazon.com/apigateway/latest/developerguide/limits.html
-  //     }
-  //   },
-  //   routes: {
-  //     'GET /version': 'packages/lambda/src/autobahn.getVersion',
-  //     'GET /error': 'packages/lambda/src/autobahn.getError',
-  //     'GET /': 'packages/lambda/src/autobahn.getHome'
-  //   }
-  // })
   stack.setDefaultFunctionProps({
     environment: {
       SENTRY_DSN,
