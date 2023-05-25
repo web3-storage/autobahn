54b4bb6105badd222fbc6414579f77fd1fc6bf6f
Alan Shaw <alan.shaw@protocol.ai>
2023-05-25 22:48:23 +0100

    fix: try node20

--- packages/core/test/index.spec.js
+++ packages/core/test/index.spec.js
@@ -1,121 +1,134 @@
 /* eslint-env browser */
 import anyTest from 'ava'
-// import autobahn from '../src/index.js'
-// import { Builder } from './helpers/builder.js'
-// import { createDynamo, createDynamoTable, createS3, createS3Bucket } from './helpers/aws.js'
+import { randomBytes } from 'node:crypto'
+import { equals } from 'multiformats/bytes'
+import { CarReader } from '@ipld/car'
+import autobahn from '../src/index.js'
+import { Builder } from './helpers/builder.js'
+import { createDynamo, createDynamoTable, createS3, createS3Bucket } from './helpers/aws.js'
 
-// /**
-//  * @typedef {{
-//  *   s3: import('./helpers/aws').TestAwsService<import('@aws-sdk/client-s3').S3Client>
-//  *   s3Bucket: string
-//  *   dynamo: import('./helpers/aws').TestAwsService<import('@aws-sdk/client-dynamodb').DynamoDBClient>
-//  *   dynamoTable: string
-//  *   builder: Builder
-//  *   dispatchFetch: ReturnType<createFetchDispatcher>
-//  * }} TestContext
-//  */
+/**
+ * @typedef {{
+ *   s3: import('./helpers/aws').TestAwsService<import('@aws-sdk/client-s3').S3Client>
+ *   s3Bucket: string
+ *   dynamo: import('./helpers/aws').TestAwsService<import('@aws-sdk/client-dynamodb').DynamoDBClient>
+ *   dynamoTable: string
+ *   builder: Builder
+ *   dispatchFetch: ReturnType<createFetchDispatcher>
+ * }} TestContext
+ */
 
-// const test = /** @type {import('ava').TestFn<TestContext>} */ (anyTest)
-const test = anyTest
+const test = /** @type {import('ava').TestFn<TestContext>} */ (anyTest)
 
-// test.before(async t => {
-//   t.context.s3 = await createS3()
-//   t.context.dynamo = await createDynamo()
-// })
-
-// test.beforeEach(async t => {
-//   t.context.s3Bucket = await createS3Bucket(t.context.s3.client)
-//   t.context.dynamoTable = await createDynamoTable(t.context.dynamo.client)
-//   t.context.builder = new Builder(t.context.dynamo.client, t.context.dynamoTable, t.context.s3.client, t.context.s3.region, t.context.s3Bucket)
-//   t.context.dispatchFetch = createFetchDispatcher(t.context)
-// })
-
-// test.after(async t => {
-//   try {
-//     await Promise.all([
-//       t.context.s3.container.stop(),
-//       t.context.dynamo.container.stop()
-//     ])
-//   } catch (err) {
-//     console.error(err)
-//   }
-// })
-
-test('a test', async t => {
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  t.true(true)
+test.before(async t => {
+  t.context.s3 = await createS3()
+  t.context.dynamo = await createDynamo()
 })
 
-// test('should get a file', async t => {
-//   const input = new Blob([randomBytes(256)])
-//   const root = await t.context.builder.add(input)
+test.beforeEach(async t => {
+  t.context.s3Bucket = await createS3Bucket(t.context.s3.client)
+  t.context.dynamoTable = await createDynamoTable(t.context.dynamo.client)
+  t.context.builder = new Builder(t.context.dynamo.client, t.context.dynamoTable, t.context.s3.client, t.context.s3.region, t.context.s3Bucket)
+  t.context.dispatchFetch = createFetchDispatcher(t.context)
+})
 
-//   const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}`)
-//   if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
+test.after(async t => {
+  await t.context.s3.container.stop()
+  await t.context.dynamo.container.stop()
+})
 
-//   await sameBytes(t, res, input)
-// })
+test('should get a file', async t => {
+  const input = new Blob([randomBytes(256)])
+  const root = await t.context.builder.add(input)
 
-// test('should get a file in a directory', async t => {
-//   const input = [
-//     new File([randomBytes(256)], 'data.txt'),
-//     new File([randomBytes(512)], 'image.png')
-//   ]
-//   const root = await t.context.builder.add(input)
+  const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}`)
+  if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
 
-//   const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}/${input[0].name}`)
-//   if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
+  await sameBytes(t, res, input)
+})
 
-//   await sameBytes(t, res, input[0])
-// })
+test('should get a file in a directory', async t => {
+  const input = [
+    new File([randomBytes(256)], 'data.txt'),
+    new File([randomBytes(512)], 'image.png')
+  ]
+  const root = await t.context.builder.add(input)
 
-// test('should get a big file', async t => {
-//   const input = [new File([randomBytes(609261780)], 'sargo.tar.xz')]
-//   const root = await t.context.builder.add(input)
+  const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}/${input[0].name}`)
+  if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
 
-//   const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}/${input[0].name}`)
-//   if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
+  await sameBytes(t, res, input[0])
+})
 
-//   await sameBytes(t, res, input[0])
-// })
+test('should get a big file', async t => {
+  const input = [new File([randomBytes(609261780)], 'sargo.tar.xz')]
+  const root = await t.context.builder.add(input)
 
-// test('should get a CAR via Accept headers', async t => {
-//   const input = new Blob([randomBytes(256)])
-//   const root = await t.context.builder.add(input)
+  const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}/${input[0].name}`)
+  if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
 
-//   const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}`, {
-//     headers: { Accept: 'application/vnd.ipld.car;order=dfs;' }
-//   })
-//   if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
+  await sameBytes(t, res, input[0])
+})
 
-//   const contentType = res.headers.get('Content-Type')
-//   t.true(contentType?.includes('application/vnd.ipld.car'))
-//   t.true(contentType?.includes('order=dfs'))
+test('should get a CAR via Accept headers', async t => {
+  const input = new Blob([randomBytes(256)])
+  const root = await t.context.builder.add(input)
 
-//   const output = new Uint8Array(await res.arrayBuffer())
-//   await t.notThrowsAsync(CarReader.fromBytes(output))
-// })
+  const res = await t.context.dispatchFetch(`http://localhost:8787/ipfs/${root}`, {
+    headers: { Accept: 'application/vnd.ipld.car;order=dfs;' }
+  })
+  if (!res.ok) t.fail(`unexpected response: ${await res.text()}`)
 
-// /** @param {TestContext} context */
-// function createFetchDispatcher (context) {
-//   /**
-//    * @param {URL|string} url
-//    * @param {RequestInit} [init]
-//    */
-//   return (url, init) => {
-//     const request = new Request(url, init)
-//     const env = {
-//       DEBUG: 'true',
-//       AWS_ACCESS_KEY_ID: context.s3.credentials?.accessKeyId,
-//       AWS_SECRET_ACCESS_KEY: context.s3.credentials?.secretAccessKey,
-//       DYNAMO_ENDPOINT: context.dynamo.endpoint,
-//       DYNAMO_REGION: context.dynamo.region,
-//       DYNAMO_TABLE: context.dynamoTable,
-//       S3_ENDPOINT: context.s3.endpoint,
-//       S3_REGIONS: context.s3.region,
-//       PREFER_REGION: context.s3.region
-//     }
-//     const ctx = { waitUntil: () => {} }
-//     return autobahn.fetch(request, env, ctx)
-//   }
-// }
+  const contentType = res.headers.get('Content-Type')
+  t.true(contentType?.includes('application/vnd.ipld.car'))
+  t.true(contentType?.includes('order=dfs'))
+
+  const output = new Uint8Array(await res.arrayBuffer())
+  await t.notThrowsAsync(CarReader.fromBytes(output))
+})
+
+/** @param {TestContext} context */
+function createFetchDispatcher (context) {
+  /**
+   * @param {URL|string} url
+   * @param {RequestInit} [init]
+   */
+  return (url, init) => {
+    const request = new Request(url, init)
+    const env = {
+      DEBUG: 'true',
+      AWS_ACCESS_KEY_ID: context.s3.credentials?.accessKeyId,
+      AWS_SECRET_ACCESS_KEY: context.s3.credentials?.secretAccessKey,
+      DYNAMO_ENDPOINT: context.dynamo.endpoint,
+      DYNAMO_REGION: context.dynamo.region,
+      DYNAMO_TABLE: context.dynamoTable,
+      S3_ENDPOINT: context.s3.endpoint,
+      S3_REGIONS: context.s3.region,
+      PREFER_REGION: context.s3.region
+    }
+    const ctx = { waitUntil: () => {} }
+    return autobahn.fetch(request, env, ctx)
+  }
+}
+
+/**
+ * @param {import('ava').ExecutionContext<TestContext>} t
+ * @param {{ arrayBuffer (): Promise<ArrayBuffer> }} a
+ * @param {{ arrayBuffer (): Promise<ArrayBuffer> }} b
+ */
+async function sameBytes (t, a, b) {
+  const aBytes = new Uint8Array(await a.arrayBuffer())
+  const bBytes = new Uint8Array(await b.arrayBuffer())
+  t.true(equals(aBytes, bBytes))
+}
+
+class File extends Blob {
+  /**
+   * @param {BlobPart[]} parts
+   * @param {string} name
+   */
+  constructor (parts, name) {
+    super(parts)
+    this.name = name
+  }
+}
