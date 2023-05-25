f9f0333e4081f7266383af6250f606e61cdabe6e
Alan Shaw <alan.shaw@protocol.ai>
2023-05-25 23:00:59 +0100

    fix: use older clients

--- packages/core/src/bindings.d.ts
+++ packages/core/src/bindings.d.ts
@@ -25,33 +25,3 @@ export type RegionalS3Clients = Record<string, S3Client>
 export interface S3Context extends Context {
   s3Clients: RegionalS3Clients
 }
-
-export interface R2GetOptions {
-  range?: {
-    offset: number
-    length?: number
-  }
-}
-
-export interface R2ListOptions {
-  prefix?: string
-  cursor?: string
-}
-
-export interface R2Object {
-  body: ReadableStream
-  size: number
-  key: string
-}
-
-export interface R2Objects {
-  objects: R2Object[]
-  truncated: boolean
-  cursor?: string
-}
-
-export interface R2Bucket {
-  get (k: string, o?: R2GetOptions): Promise<R2Object | null>
-  head (k: string, o?: R2GetOptions): Promise<R2Object | null>
-  list (o?: R2ListOptions): Promise<R2Objects | null>
-}
